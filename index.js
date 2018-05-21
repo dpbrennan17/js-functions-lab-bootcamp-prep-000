1ad028ae96426b6165482aa70c9bcabdb7cf5fac
David Brennan <d.p.brennan@tcu.edu>
2018-05-21 04:08:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo( name ) {
-  return `Happy Holidays, ${name}!`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo( holiday , name ) {
-  return `Happy ${holiday} , ${name}!`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown( holiday , days ) {
